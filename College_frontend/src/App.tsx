9df9db576911d72eee3dc3bfb6569ac60ba381ca
Abhinav Mohan <abhinavmohan453@gmail.com>
2026-05-17 17:58:29 +0530

    changes

--- College_frontend/src/App.tsx
+++ College_frontend/src/App.tsx
@@ -1,17 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/login/login.tsx";
-import UserHome from "./pages/student/userHome.tsx";
+
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/userhome" element={<UserHome />} />
-      </Routes>
-    </Router>
-  );
+  return (<h2>College App</h2>);
 }
 
 export default App;
