eb23f013413189b143c3dc42756525fee9762730
Abhinav Mohan <abhinav.m@coddletech.com>
2026-05-17 17:28:01 +0530

    Create App.tsx

---
+++ College_frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/login/login.tsx";
+import UserHome from "./pages/student/userHome.tsx";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/userhome" element={<UserHome />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
